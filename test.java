f1b1ca7b86ca0aaf1731b1ac29c78167206517d3
jaimejahuey <jahuey777@hotmail.com>
2018-01-08 19:45:51 -0500

    changes

--- test.java
+++ test.java
@@ -1,8 +1,11 @@
 public class Testing{
 
 	private String Name; 
-    private String danny;
+    private String DANNY;
     private String most;
     private String moist; 
     
+    public String getName(){
+        return DANNY;
+    }
 }
