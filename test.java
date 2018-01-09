957e80699db52cbd08a3ae95ccc6c5bd62b7593a
Daniel Espina <dannyj.espina@gmail.com>
2018-01-08 19:47:13 -0500

    changes

--- test.java
+++ test.java
@@ -1,8 +1,14 @@
 public class Testing{
 
 	private String Name; 
-    private String danny;
+    private String Danny;
     private String most;
     private String moist; 
     
+    private void setMost(String most) {
+        this.most = most;
+    }
+    private String getMost() {
+        return most;
+    }
 }
